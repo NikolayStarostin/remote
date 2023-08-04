de79972bb3762515a1682fe4ac63bfcafcd5be3b
Squirrel <5260850@gmail.com>
2023-08-04 15:48:47 +0300

    HomeWork Task 1

---
+++ Fix bugs/Lesson_2/Task_1.java
@@ -0,0 +1,22 @@
+package Lesson_2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("takeFloat() = " + takeFloat());
+    }
+
+    private static float takeFloat() {
+        for (;;){
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter valid number type float");
+                return scanner.nextFloat();
+            } catch (InputMismatchException e){
+                System.out.println("Input was not number type float");
+            }
+        }
+    }
+}
